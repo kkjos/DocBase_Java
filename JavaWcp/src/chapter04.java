850f761806f1b4c8a437a1fc9fd2a6ac31cef029
kkjos <k19951227@gmail.com>
2022-02-01 12:31:46 +0900

    [finish]chapter4

---
+++ JavaWcp/src/chapter04.java
@@ -0,0 +1,15 @@
+
+public class chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "kkjos";
+		System.out.println(str1);
+		
+		str1 = "jooh";
+		System.out.println(str1);
+		
+		final int lukky_number = 7 ;
+		System.out.println(lukky_number);
+	}
+
+}
