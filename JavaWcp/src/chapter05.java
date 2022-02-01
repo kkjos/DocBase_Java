fefcc3dac7eaed5f10e8afc4c952284aa4f9723f
kkjos <k19951227@gmail.com>
2022-02-01 12:56:17 +0900

    [finish]chapter5

---
+++ JavaWcp/src/chapter05.java
@@ -0,0 +1,23 @@
+
+public class chapter05 {
+	public static void main(String[] args) {
+		long l = 10000000;
+		System.out.println(l);
+		double d = 3.14;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(d);
+		
+		Float f = 3.14f;
+		System.out.println(f);
+		Integer i = 2;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+		
+		int[] array = {1,2,3,4,5};
+		System.out.println(array[3]);
+		
+	}
+
+}
