fe041d28c44a0b7eeac8c48e90f2637f1cd4aec7
kkjos <k19951227@gmail.com>
2022-02-02 10:43:19 +0900

    [finish]chapter7-test2

--- JavaWcp/src/chapter07.java
+++ JavaWcp/src/chapter07.java
@@ -11,6 +11,22 @@ public class chapter07 {
 		}else {
 			System.out.println("とても遠いです");
 		}
+		
+		String signal = "red";
+		switch(signal) {
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		case "yellow":
+			System.out.println("黄信号です");
+			break;
+		case "blue":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の入りではありません");
+		}
+		
 	}
 
 }
