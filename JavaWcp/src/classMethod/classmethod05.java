0dc7340f0fa179f6bc3b7c4077f17a68ac2f29cd
kkjos <k19951227@gmail.com>
2022-02-04 11:12:25 +0900

    [finish]chapter9

--- JavaWcp/src/classMethod/classmethod05.java
+++ JavaWcp/src/classMethod/classmethod05.java
@@ -1,5 +1,10 @@
 package classMethod;
 
-public class classmethod05 {
+import classMethod.human.human05;
 
+public class classmethod05 {
+    public static void main(String[] args) {
+        human05 yamada = new human05();  
+        System.out.println("名前は" + yamada.getName() + "で、年齢は" + yamada.getAge() + "です。");
+    }
 }
--- JavaWcp/src/classMethod/human/human05.java
+++ JavaWcp/src/classMethod/human/human05.java
@@ -1,5 +1,19 @@
 package classMethod.human;
 
 public class human05 {
+	private String name;
+	private int age;
 
+	public human05() {
+		name = "山田";
+		age = 20;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
 }
